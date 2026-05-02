48a3f81bf707e85e762719339a4b8e779246317a
Oleg Polin <140200108+olegpolin@users.noreply.github.com>
2026-05-02 19:38:38 -0400

    Add privacy policy and terms of service links to footer

--- src/lib/components/footer.svelte
+++ src/lib/components/footer.svelte
@@ -7,12 +7,12 @@
   import GitHubIcon from '$lib/assets/icons/github-icon.svelte';
 </script>
 
-<footer class="w-full flex flex-row items-center justify-between gap-8 p-8">
-  <div class="flex flex-row items-center gap-4">
+<footer class="w-full flex flex-col md:flex-row items-center justify-between gap-4 p-8">
+  <div class="flex flex-col md:flex-row items-center gap-4">
     <Button variant="ghost" size="icon" href="https://github.com/yoursplit/yoursplit.com" target="_blank" rel="noopener noreferrer" aria-label="GitHub repository">
       <GitHubIcon />
     </Button>
-    <p class="text-sm max-sm:text-center text-muted-foreground">
+    <p class="text-sm text-center md:text-left text-muted-foreground">
       Created by
       <a class="font-medium underline underline-offset-4" href="https://olegpolin.com" target="_blank" rel="noopener noreferrer">
         Oleg
@@ -26,10 +26,16 @@
     </p>
   </div>
 
-  <DropdownMenu.Root>
-    <DropdownMenu.Trigger class={buttonVariants({ variant: 'outline', size: 'icon' })}>
-      <SunIcon class="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 !transition-all dark:-rotate-90 dark:scale-0" />
-      <MoonIcon class="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 !transition-all dark:rotate-0 dark:scale-100" />
+  <div class="flex items-center gap-6">
+    <div class="text-sm text-muted-foreground flex gap-4">
+      <a class="hover:underline underline-offset-4" href="/privacy">Privacy Policy</a>
+      <a class="hover:underline underline-offset-4" href="/terms">Terms of Service</a>
+    </div>
+
+    <DropdownMenu.Root>
+      <DropdownMenu.Trigger class={buttonVariants({ variant: 'outline', size: 'icon' })}>
+      <SunIcon class="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all! dark:-rotate-90 dark:scale-0" />
+      <MoonIcon class="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all! dark:rotate-0 dark:scale-100" />
       <span class="sr-only">Toggle theme</span>
     </DropdownMenu.Trigger>
     <DropdownMenu.Content align="end">
@@ -38,4 +44,5 @@
       <DropdownMenu.Item onclick={() => resetMode()}>System</DropdownMenu.Item>
     </DropdownMenu.Content>
   </DropdownMenu.Root>
+  </div>
 </footer>
